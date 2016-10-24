ded5a443e2fbbc88d190194673a78f3fa7c65838
rsstdd <rosstod@gmail.com>
2016-10-23 19:57:10 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const morgan = require('morgan');
+const path = require('path');
+
+app.disable('x-powered-by');
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+
+  case 'production':
+    app.use(morgan('short'));
+    break;
+
+  default:
+}
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use(express.static(path.join('public')));
+
+// CSRF protection
+app.use((req, res, next) => {
+  if (/json/.test(req.get('Accept'))) {
+    return next();
+  }
+
+  res.sendStatus(406);
+});
+
+
+const topics = require('./routes/topics');
+const posts = require('./routes/posts');
+const users = require('./routes/users');
+
+app.use('topics');
+app.use('posts');
+app.use('users');
+
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+app.use((err, _req, res, _next) => {
+  if (err.output && err.output.statusCode) {
+    return res
+      .status(err.output.statusCode)
+      .set('Content-Type', 'text/plain')
+      .send(err.message);
+  }
+
+  if (err.status) {
+    return res
+      .status(err.status)
+      .set('Content-Type', 'text/plain')
+      .send(err.errors[0].messages[0]);
+  }
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  if (app.get('env') !== 'test') {
+    // eslint-disable-next-line no-console
+    console.log(`Listening on port ${port}`);
+  }
+})
+
+module.exports = app;
